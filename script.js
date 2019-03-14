0e58ccea4a630652864863a494ff1de9145de851
Ketul Makwana <ketulmakwana09@gmail.com>
2019-03-14 10:04:13 +0530

    Added script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var billamt = document.getElementById("billamt");
+var serviceQuality = document.getElementById("serviceQual");
+var people = document.getElementById("people");
+var eachTip = document.getElementById("tip");
+var totalTip;
 
+function calculateTip() {
+
+    if (billamt.value === "" || serviceQuality === 0) {
+        alert("Please enter values!");
+    }
+
+    if (people.value === "" || people.value < 1) {
+        people.value = "1";
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    totalTip = (billamt.value) * (serviceQuality.value);
+    eachTip.textContent = totalTip / (people.value);
+}
